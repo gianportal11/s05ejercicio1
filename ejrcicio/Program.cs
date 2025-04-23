98e253d59f1461fac65fd6f12a84a558a522d6ff
Lucas <sihuen8@gmail.com>
2025-04-23 16:37:42 -0500

    Agregar archivos de proyecto.

---
+++ ejrcicio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejrcicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sumaCubos = 0;
+            Console.Write("Ingrese la cantidad de números: ");
+               
+
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write("Ingrese el número i: ");
+                    double numero = double.Parse(Console.ReadLine());
+                    sumaCubos += Math.Pow(numero, 3);
+                }
+
+                Console.WriteLine("La suma de los cubos es" + sumaCubos);
+            }
+        }
+
+    }
+
---
+++ s05ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s05ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n , suma;
+            Console.WriteLine("ingrese numero del elemanto: ");
+            n = int.Parse(Console.ReadLine());
+           suma = 0;
+            for (int i = 1; i <= n; i++) 
+                { Console.WriteLine("interaccion " + n );
+                suma = suma + i;
+                Console.WriteLine("en esta interaccion la suma es " + suma);
+                Console.ReadKey(); }
+
+        }
+    }
+}
